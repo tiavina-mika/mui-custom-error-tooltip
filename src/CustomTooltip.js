9aa2391bd75c447ad3afa6c110f260ce21b0791e
TiavinaMichael <ralainirinatiavina@gmail.com>
2022-03-08 17:21:19 +0300

    Initial commit

---
+++ src/CustomTooltip.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles, Typography } from '@material-ui/core';
+import InfoIcon from "@material-ui/icons/Info"
+
+const message = "Le montant minimum de commande n’est pas atteint. Il manque 18 € pour atteindre ce minimum."
+
+const useStyles = makeStyles({
+  container: {
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  productLabel: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  titleError: {
+    marginRight: 7,
+    color: '#FF5D5A',
+  },
+  errorIcon: {
+    color: '#FF5D5A',
+    cursor: 'pointer',
+  },
+  errorTooltip: {
+    backgroundColor: '#FF5D5A',
+    fontSize: 14,
+  },
+  arrow: {
+    color: '#FF5D5A',
+  },
+})
+const CustomTooltip = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.productLabel}>
+        <Typography className={classes.titleError}>
+          Montant des produits
+        </Typography>
+        <Tooltip
+          placement="top"
+          title={message}
+          enterDelay={500}
+          leaveDelay={200}
+          arrow
+          classes={{
+            tooltip: classes.errorTooltip,
+            arrow: classes.arrow,
+          }}
+        >
+            <InfoIcon className={classes.errorIcon} />
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
+
+export default CustomTooltip;
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomTooltip from './CustomTooltip';
+
+ReactDOM.render(<CustomTooltip />, document.querySelector('#root'));
+    
